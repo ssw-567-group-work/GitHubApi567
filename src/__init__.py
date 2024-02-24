ce0736af481b1cc27178a10971d7d6b4a8c928b6
Carson McManus <carson.mcmanus1@gmail.com>
2024-02-24 11:09:11 -0500

    add fetcher helper class

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 from .cli import Args
-
+from .fetcher import Fetcher
 
 def main():
     args = Args(underscores_to_dashes=True).parse_args()
+    fetcher = Fetcher()
+    # TODO: implement
 
 
 def add(a, b):
