7b693257fc1cb3444c0e0e4fddcba62872623f2a
Carson McManus <carson.mcmanus1@gmail.com>
2024-02-26 19:11:40 -0500

    remove stray newline

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,6 @@ from .fetcher import Fetcher
 def main(argv: Sequence[str] | None = None, fetcher: Fetcher = Fetcher()):
     args = Args(underscores_to_dashes=True).parse_args(argv)
     repos = fetcher.fetch_repos(args.username)
-
     for repo in repos:
         commits = fetcher.fetch_commit_count(args.username, repo)
         print(f"Repo: {repo} Number of commits: {commits}")
